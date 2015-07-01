3fd4fa291a4a272d7e762823ae127c1a59add8aa
Gustavo ParesY <gustavoanalytics@gmail.com>
2015-07-01 10:46:43 -0500

    added recent videos to ui

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
   Category.create(name: "TV Shows")
   Category.create(name: "Other Shows")
 
-  Video.create( title: 'Futurama', 
+  Video.create( title: 'Futurama new', 
                 description: "Futurama is an American adult animated science fiction sitcom created by Matt Groening for the Fox Broadcasting Company. The series follows the adventures of a late-20th-century New York City pizza delivery boy, Philip J. Fry, who, after being unwittingly cryogenically frozen for one thousand years, finds employment at Planet Express, an interplanetary delivery company in the retro-futuristic 31st century. The series was envisioned by Groening in the late 1990s while working on The Simpsons, later bringing Cohen aboard to develop storylines and characters to pitch the show to Fox.",
                 small_cover_url: "tmp/futurama.jpg",
                 large_cover_url: "tmp/monk_large.jpg",
@@ -17,26 +17,26 @@
               )
 
 
-  Video.create( title: 'South Park', 
+  Video.create( title: 'South Park new', 
                 description: "South Park is an American adult animated sitcom created by Trey Parker and Matt Stone for the Comedy Central television network. Intended for mature audiences, the show has become infamous for its crude language and dark, surreal humor that satirizes a wide range of topics. The ongoing narrative revolves around four boys—Stan Marsh, Kyle Broflovski, Eric Cartman, and Kenny McCormick—and their bizarre adventures in and around the titular Colorado town. Much like The Simpsons, South Park utilizes a very large ensemble cast of recurring characters.",
                 small_cover_url: "tmp/south_park.jpg",
                 large_cover_url: "tmp/monk_large.jpg",
                 category_id: 1
               )
 
-  Video.create( title: 'South Park', 
+  Video.create( title: 'South Park 2', 
                 description: "South Park is an American adult animated sitcom created by Trey Parker and Matt Stone for the Comedy Central television network. Intended for mature audiences, the show has become infamous for its crude language and dark, surreal humor that satirizes a wide range of topics. The ongoing narrative revolves around four boys—Stan Marsh, Kyle Broflovski, Eric Cartman, and Kenny McCormick—and their bizarre adventures in and around the titular Colorado town. Much like The Simpsons, South Park utilizes a very large ensemble cast of recurring characters.",
                 small_cover_url: "tmp/south_park.jpg",
                 large_cover_url: "tmp/monk_large.jpg",
                 category_id: 2
               )
-  Video.create( title: 'South Park', 
+  Video.create( title: 'South Park 3', 
                 description: "South Park is an American adult animated sitcom created by Trey Parker and Matt Stone for the Comedy Central television network. Intended for mature audiences, the show has become infamous for its crude language and dark, surreal humor that satirizes a wide range of topics. The ongoing narrative revolves around four boys—Stan Marsh, Kyle Broflovski, Eric Cartman, and Kenny McCormick—and their bizarre adventures in and around the titular Colorado town. Much like The Simpsons, South Park utilizes a very large ensemble cast of recurring characters.",
                 small_cover_url: "tmp/south_park.jpg",
                 large_cover_url: "tmp/monk_large.jpg",
                 category_id: 2
               )
-  Video.create( title: 'South Park', 
+  Video.create( title: 'South Park 4', 
                 description: "South Park is an American adult animated sitcom created by Trey Parker and Matt Stone for the Comedy Central television network. Intended for mature audiences, the show has become infamous for its crude language and dark, surreal humor that satirizes a wide range of topics. The ongoing narrative revolves around four boys—Stan Marsh, Kyle Broflovski, Eric Cartman, and Kenny McCormick—and their bizarre adventures in and around the titular Colorado town. Much like The Simpsons, South Park utilizes a very large ensemble cast of recurring characters.",
                 small_cover_url: "tmp/south_park.jpg",
                 large_cover_url: "tmp/monk_large.jpg",
