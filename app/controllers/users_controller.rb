072d93cc0333167914dce8cf469a3d7fff88ba1c
Gustavo ParesY <gustavoanalytics@gmail.com>
2015-07-03 09:31:29 -0500

    added strong params to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,19 @@ class UsersController < ApplicationController
 		@user = User.new		
 	end
 	def create
+		@user = User.create(params[user_params])
 		binding.pry
-		@user = User.create(params[:user])
 		if @user.save
 			redirect_to root_path
 		else
 			redirect_to register_path
 		end
+	end
+
+private
+	
+	def user_params
+		params.require(:user).permit(:username, :email, :password)
 	end
 
 end
