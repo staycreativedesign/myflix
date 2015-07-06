a7bdb01ab9f8d83c63e9f768ed1baf6bd6e01ce7
Gustavo ParesY <gustavoanalytics@gmail.com>
2015-07-05 23:57:16 -0500

    added redirect_to root_path and removed, validations: false

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.create(user_params)
 		if @user.save
-			redirect_to sign_in_path
+			redirect_to root_path
 		else
 			render :new
 		end
