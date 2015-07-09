ecc39bba3df95956bbbab558a8b0977b5431d87e
Gustavo ParesY <gustavoanalytics@gmail.com>
2015-07-09 01:14:11 -0500

    Implemented reviews with review controller, video show, review controller spec

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,14 @@
 class ReviewsController < ApplicationController
-
+	before_filter :require_user
+	
 	def create
 		@video = Video.find(params[:video_id])
-		@review = @video.reviews.create(review_params)
+		@review = @video.reviews.create(review_params.merge(user: current_user))
 		if @review.save
 			redirect_to video_path(@video), notice: 'Review created'
 		else
 			flash[:error] = 'Review not created missing description.'
-			redirect_to video_path(@video)
+			render "videos/show"
 		end
 	end
 
--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -2,30 +2,48 @@ require 'spec_helper'
 
 describe ReviewsController do
 	describe "POST create" do
-		let(:alice) { Fabricate(:user) }
 		let(:video) { Fabricate(:video) }
-		context "with valid input" do
-		before { post :create, user: alice, video_id: video.id, review: Fabricate.attributes_for(:review) }	
-			it "sets the @review record when input is valid" do
-				expect(video.reviews.count).to eq(1)
+		
+		context "with authenticated users" do
+			let(:current_user) { Fabricate(:user) }
+			before { session[:user_id] = current_user.id }
+			context "with valid input" do
+			before { post :create, user: current_user, video_id: video.id, review: Fabricate.attributes_for(:review) }	
+				it "creates a review" do
+					expect(video.reviews.count).to eq(1)
+				end
+				it "creates a review associated with a video" do
+					expect(video.reviews.first.video).to eq(video)
+				end
+				it "creates a review associated to a user" do
+					expect(video.reviews.first.user).to eq(current_user)
+				end
+
+				it "redirects to the video show page" do
+					expect(response).to redirect_to video_path(video)
+				end
+				it "sets flash[:notice]" do
+					expect(flash[:notice]).not_to be_blank
+				end
 			end
-			it "redirects to the @video path" do
-				expect(response).to redirect_to video_path(video)
-			end
-			it "sets flash[:notice]" do
-				expect(flash[:notice]).not_to be_blank
-			end
+
+			context "with invalid input" do
+				before { post :create, user: current_user, video_id: video.id, review: Fabricate.attributes_for(:review, description: nil) }
+				it "does not set @review when input is invalid" do
+					expect(video.reviews.count).to eq(0)
+				end
+				it "sets flash[:error]" do
+					expect(flash[:error]).not_to be_blank
+				end
+				it "renders video/show template" do
+					expect(response).to render_template "videos/show"
+				end
+			end	
 		end
-		context "with invalid input" do
-			before { post :create, user: alice, video_id: video.id, review: Fabricate.attributes_for(:review, description: nil) }
-			it "does not set @review when input is invalid" do
-				expect(video.reviews.count).to eq(0)
-			end
-			it "sets flash[:error]" do
-				expect(flash[:error]).not_to be_blank
-			end
-			it "redirects to the @video path" do
-				expect(response).to redirect_to video_path(video)
+		context "with unathenticated users" do
+			before { post :create, user: nil, video_id: video.id, review: Fabricate.attributes_for(:review) }	
+			it "redirects to the sign in page" do
+				expect(response).to redirect_to sign_in_path
 			end
 		end
 	end
