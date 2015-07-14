911f78689c733b9e19545af423b76f2e04b3dd39
Gustavo ParesY <gustavoanalytics@gmail.com>
2015-07-14 11:42:19 -0500

    added delegate to review, user_full_name, added video#show to with user_full_name

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,7 @@ class Review < ActiveRecord::Base
 	belongs_to :video
 	belongs_to :user
 	validates :description, presence: true
+
+	delegate :full_name, :to => :user, :prefix => :user
+
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,4 +4,5 @@ describe Review do
 	it { should belong_to :video }
 	it { should belong_to :user }
 	it { should validate_presence_of :description}
+	it { should delegate_method(:full_name).to(:user).with_prefix(:user) }
 end
