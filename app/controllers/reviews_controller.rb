d0a994bdd0dab435ae81d15fdaba713cbe642d95
Gustavo ParesY <gustavoanalytics@gmail.com>
2015-07-15 23:51:32 -0500

    added where 1 user can only have 1 review for 1 video, changed tabs to spaces on usercontroller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,13 +2,13 @@ class ReviewsController < ApplicationController
   before_filter :require_user
   
   def create
-    @video = Video.find(params[:video_id])
-    @reviews = @video.reviews
-    @review = @video.reviews.create(review_params.merge(user: current_user))
-    if @review.save
-      redirect_to video_path(@video), notice: 'Review created'      
+    video = Video.find(params[:video_id])
+    reviews = video.reviews
+    review = video.reviews.create(review_params.merge(user: current_user))
+    if review.save
+      redirect_to video_path(video), notice: 'Review created'      
     else
-      flash[:error] = @review.errors.full_messages.to_sentence
+      flash[:error] = review.errors.full_messages.to_sentence
       render "videos/show"
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,22 @@
 class UsersController < ApplicationController
 
-	def new
-		@user = User.new		
-	end
-	def create
-		@user = User.create(user_params)
-		if @user.save
-			redirect_to root_path
-		else
-			flash[:error] = 'You email or password does not match'
-			render :new
-		end
-	end
+  def new
+    @user = User.new    
+  end
+  def create
+    user = User.create(user_params)
+    if user.save
+      redirect_to root_path
+    else
+      flash[:error] = 'You email or password does not match'
+      render :new
+    end
+  end
 
 private
-	
-	def user_params
-		params.require(:user).permit(:full_name, :email, :password)
-	end
+  
+  def user_params
+    params.require(:user).permit(:full_name, :email, :password)
+  end
 
 end
